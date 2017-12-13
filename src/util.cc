b81d0c5f8b4af80003ba5cda58625f7d34d2af7e
faco <f4co@yahoo.com>
2017-12-14 00:05:52 +0700

    add print time

--- src/util.cc
+++ src/util.cc
@@ -28,7 +28,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
+void print_time()
+{
+	time_t	timer;
+	struct tm* tm_info;
+	char buffer[26];
+
+	time(&timer);
+	tm_info = localtime(&timer);
+	strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+	printf(buffer);
+}
 
 void display_freq(float f) {
 
